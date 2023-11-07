7c91f41edb1ec1489d2170b02a5bd9eb583b654d
Jaak Peldes <jaak@taxscouts.com>
2023-11-07 22:23:17 +0200

    Install DaisyUI, Jest, @tailwind/typography,

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,37 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [require('@tailwindcss/typography'), require('daisyui')],
+
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: '#2980b9',
+                    secondary: '#8e44ad',
+                    accent: '#16a085',
+                    neutral: '#2a323c',
+                    'base-100': '#1d232a',
+                    info: '#45aaf2',
+                    success: '#2ecc71',
+                    warning: '#f1c40f',
+                    error: '#e74c3c',
+                },
+            },
+        ],
+    },
 }
 export default config
